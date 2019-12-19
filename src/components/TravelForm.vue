4c5d2eefa1c5621a965dc8156d4ffdfe98a52b1b
Joshua Hysong <joshuahysong@gmail.com>
2019-12-18 21:54:13 -0500

    Added vue multi-select and locaiton selection boxes

--- src/components/TravelForm.vue
+++ src/components/TravelForm.vue
@@ -7,8 +7,14 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-4 offset-3">
-                    <input v-model="travel.distance" type="number" class="form-control" />
+                <div class="col">
+                    <multiselect v-model="fromLocation" :options="travel.locations" :searchable="false" :close-on-select="true" placeholder="Pick a starting location"></multiselect>
+                </div>
+                <div class="col">
+                    <multiselect v-model="toLocation" :options="travel.locations" :searchable="false" :close-on-select="true" placeholder="Pick a starting location"></multiselect>
+                </div>
+                <div class="col">
+                    <input v-model="travel.distance" type="number" class="form-control" :disabled="toLocation !== 'Custom'" />
                 </div>
                 <div class="col-2">
                     <button type="submit" class="btn btn-primary">Calculate</button>
@@ -19,11 +25,18 @@
 </template>
 
 <script>
+import Multiselect from 'vue-multiselect'
 export default {
     name: "travel-form",
+    components: {
+        Multiselect
+    },
     data() {
         return {
+            fromLocation: 'Custom',
+            toLocation: 'Custom',
             travel: {
+                locations: ['Custom'],
                 distance: 42287791
             }
         }
@@ -31,6 +44,18 @@ export default {
     methods: {
         handleSubmit() {
             this.$emit('calculate:travel', this.travel.distance);
+        }
+    },
+    watch: {
+        fromLocation: function(newValue) {
+            if (newValue === 'Custom' && this.toLocation !== 'Custom') {
+                this.toLocation = 'Custom';
+            }
+        },
+        toLocation: function(newValue) {
+            if (newValue === 'Custom' && this.fromLocation !== 'Custom') {
+                this.fromLocation = 'Custom';
+            }
         }
     }
 }
