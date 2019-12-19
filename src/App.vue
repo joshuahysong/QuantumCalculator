a178354a57d57c582d76d254085e8d37fdb96f00
Joshua Hysong <jhysong@rhsinc.com>
2019-12-18 20:58:38 -0500

    Updated favicon

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-<main id="app" class="container">
-    <h1 class="mb-5">Quantum Drive Calculator</h1>
-    <travel-form @calculate:travel="calculateTravel" class="mb-3" />
-    <p class="mb-3 small"><em>Travel times are estimations and may not be 100% accurate to actual travel time in game until the underlying calculations are better understood.</em></p>
-    <drives-table :distance="distance" />
-</main>
+<div id="app">
+    <div class="container">
+        <h1 class="mb-5">Quantum Drive Calculator</h1>
+        <travel-form @calculate:travel="calculateTravel" class="mb-3" />
+        <p class="mb-3 small"><em>Travel times are estimations and may not be 100% accurate to actual travel time in game until the underlying calculations are better understood.</em></p>
+    </div>
+    <div class="container-fluid">  
+        <drives-table :distance="distance" />
+    </div>
+</div>
 </template>
 
 <script>
